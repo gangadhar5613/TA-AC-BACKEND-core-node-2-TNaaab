50fc699e645c15008883f38f31384fee6033eb8a
Narapureddy Gangadhar Reddy <gangadhar.5613@gmail.com>
2020-12-06 22:40:57 +0530

    Complete

---
+++ block-BNaaax/app/server.js
@@ -0,0 +1,10 @@
+console.log(__dirname);
+const path = require("path");
+
+let filePath = './server.js';
+
+let pathJoin = path.join(__dirname,'server.js')
+
+console.log(filePath);
+console.log(__dirname);
+console.log(pathJoin);
